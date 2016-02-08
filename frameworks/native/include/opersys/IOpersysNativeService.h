40968e4b88cd88974f011c8da21240c45c8595d3
Karim Yaghmour <karim.yaghmour@opersys.com>
2016-02-08 06:35:36 +0200

    Fix first commit

--- frameworks/native/include/opersys/IOpersysNativeService.h
+++ frameworks/native/include/opersys/IOpersysNativeService.h
@@ -1,5 +1,6 @@
 /*
  * (C) 2007 The Android Open Source Project
+ * (C) 2013-2016 Opersys inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
